a7572bcd75c91142f381ee50a8478988e789bbe3
Victor Pedrazzi <vspedrazzi@gmail.com>
2019-04-20 21:30:51 -0300

    Add assets and change background

--- client.js
+++ client.js
@@ -17,7 +17,7 @@ function init(bundle, parent, options = {}) {
   );
 
   // Load the initial environment
-  r360.compositor.setBackground(r360.getAssetURL('360_world.jpg'));
+  r360.compositor.setBackground(r360.getAssetURL('background.png'));
 }
 
 window.React360 = {init};
